f403701e631716a4f250cdaae4cd713f2d09b5af
dahabu saidi <dahabusaidi@gmail.com>
2019-05-27 14:42:28 +0300

    chunk projects panel

---
+++ resources/views/home/partials/projects.blade.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkawa
+ * Date: 5/27/19
+ * Time: 2:37 PM
+ */
+?>
+<section class="projects-area pb-100" id="project" style="margin-top: 30px">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-60 col-lg-8">
+                <div class="title text-center">
+                    <h1 class="mb-10">Why Choose Us Your Fitness Builder</h1>
+                    <p>Who are in extremely love with eco friendly system.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="active-project-carusel col-lg-12">
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house3.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house4.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house5.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house6.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house7.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house8.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house9.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project" style="background: url({{ asset('images/house10.jpg') }});">
+                        <div class="overlay overlay-content">
+                            <h1>Alex Complex for <br> esidence</h1>
+                            <p>
+                                LCD screens are uniquely modern in style, and the liquid crystals that make them work have allowed humanity to create  slimmer, more portable technology than we’ve ever had access to before. From your wrist watch to your laptop, a lot of the on the go electronics that we tote from place to place are only possible because of their thin, light LCD display screens. Liquid crystal display.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
