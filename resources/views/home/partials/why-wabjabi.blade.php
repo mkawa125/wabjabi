b61adc868e5b369a6327ee05c71f1b1caaf60dcc
dahabu saidi <dahabusaidi@gmail.com>
2019-05-28 11:55:00 +0300

    chunk why-us banner

---
+++ resources/views/home/partials/why-wabjabi.blade.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkawa
+ * Date: 5/28/19
+ * Time: 11:47 AM
+ */
+?>
+<section class="service-area" id="service" style="margin-bottom: 80px">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8 pb-40 header-text">
+                <h1 style="font-size: 25px; font-family: cursive">Why wabjabi should be your best choice</h1>
+                <p style="font-family: cursive">
+                    <strong>Who are in extremely love with eco friendly system.</strong>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 pb-30">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-user"></span>Expert Technicians</h4>
+                    <p>
+                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-30">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-license"></span>Professional Service</h4>
+                    <p>
+                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 pb-30">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-phone"></span>Great Support</h4>
+                    <p>
+                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-rocket"></span>Technical Skills</h4>
+                    <p>
+                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-diamond"></span>Highly Recomended</h4>
+                    <p>
+                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-bubble"></span>Positive Reviews</h4>
+                    <p>
+                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,70 +44,7 @@
 @include('home.partials.banner')
 @include('home.partials.activities')
 @include('home.partials.projects')
-<!-- Start service Area -->
-<section class="service-area section-gap" id="service">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-8 pb-40 header-text">
-                <h1>Why we are the best</h1>
-                <p>
-                    Who are in extremely love with eco friendly system.
-                </p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-6 pb-30">
-                <div class="single-service">
-                    <h4><span class="lnr lnr-user"></span>Expert Technicians</h4>
-                    <p>
-                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 pb-30">
-                <div class="single-service">
-                    <h4><span class="lnr lnr-license"></span>Professional Service</h4>
-                    <p>
-                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 pb-30">
-                <div class="single-service">
-                    <h4><span class="lnr lnr-phone"></span>Great Support</h4>
-                    <p>
-                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="single-service">
-                    <h4><span class="lnr lnr-rocket"></span>Technical Skills</h4>
-                    <p>
-                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="single-service">
-                    <h4><span class="lnr lnr-diamond"></span>Highly Recomended</h4>
-                    <p>
-                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="single-service">
-                    <h4><span class="lnr lnr-bubble"></span>Positive Reviews</h4>
-                    <p>
-                        Usage of the Internet is becoming more common due to rapid advancement of technology and power.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
+@include('home.partials.why-wabjabi')
 @include('home.partials.team')
 @include('home.partials.contact-us')
 @include('home.partials.footer')
