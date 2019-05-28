855489defebed1e655dab1cb43f09ef80fcac2fe
dahabu saidi <dahabusaidi@gmail.com>
2019-05-28 11:46:14 +0300

    change background of project boxes

--- resources/views/home/partials/projects.blade.php
+++ resources/views/home/partials/projects.blade.php
@@ -18,99 +18,133 @@
         </div>
         <div class="row">
             <div class="active-project-carusel col-lg-12">
-                <div class="item" style="background-color: #27A7AA">
+                <div class="item">
                     <div class="single-project">
                         <img src="{{ asset('images/house3.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
+                        <div class="project-details" style="padding: 25px">
                             <h4 style="margin-bottom: 10px">Viwanja</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Kimbiji - Mlomi
-                            </p>
-                            <h1 style="font-size: small; margin-bottom: 100px">TZSH 25,000/= @ sqm 1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="item" style="background-color: #27A7AA">
-                    <div class="single-project">
-                        <img src="{{ asset('images/house4.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
-                            <h4 style="margin-bottom: 10px">Viwanja</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Cheka - kigamboni
-                            </p>
-                            <h1 style="font-size: small; margin-bottom: 10px">TZSH 20,000/= @ sqm 1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="item" style="background-color: #27A7AA">
-                    <div class="single-project">
-                        <img src="{{ asset('images/house5.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
-                            <h4 style="margin-bottom: 10px">Viwanja</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> MWONGOZO
-                            </p>
-                            <h1 style="font-size: small; margin-bottom: 10px">TZSH 40,000/= @ sqm 1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="item" style="background-color: #27A7AA">
-                    <div class="single-project">
-                        <img src="{{ asset('images/house6.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
-                            <h4 style="margin-bottom: 10px">Viwanja - Dege</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Dege, Kigamboni, Dar es salaam
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">Kimbiji - Mlomi</strong>
                             </p>
                             <h1 style="font-size: small; margin-bottom: 10px">TZSH 25,000/= @ sqm 1</h1>
+                            <p>
+                                Mradi upo km 1 kutoka kiwanda cha lake cement.
+                                Eneo lipo umbali wa mita 270 kutoka kwenye barabara kuu kimbiji- kivukoni:
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="item" style="background-color: #27A7AA">
+                <div class="item">
+                    <div class="single-project">
+                        <img src="{{ asset('images/house4.jpg') }}" alt="" >
+                        <div class="project-details" style="padding: 25px">
+                            <h4 style="margin-bottom: 10px">Viwanja</h4>
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger"> Cheka - kigamboni</strong>
+                            </p>
+                            <h1 style="font-size: small; margin-bottom: 10px">TZSH 20,000/= @ sqm 1</h1>
+                            <p>
+                                Mradi upo km 1 kutoka barabara kuu ya kigamboni – kimbiji, umeme,
+                                maji ,shule ya msingi na awali , pamoja na chuo kipo karibu.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project">
+                        <img src="{{ asset('images/house5.jpg') }}" alt="" >
+                        <div class="project-details" style="padding: 25px">
+                            <h4 style="margin-bottom: 10px">Viwanja</h4>
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">MWONGOZO</strong>
+                            </p>
+                            <h1 style="font-size: small; margin-bottom: 10px">TZSH 40,000/= @ sqm 1</h1>
+                            <p>
+                                Mradi upo mita 150 kutoka bahari ya hindi
+                                Umeme, maji , shule ya msingi na sekondari
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="single-project">
+                        <img src="{{ asset('images/house6.jpg') }}" alt="" >
+                        <div class="project-details" style="padding: 25px">
+                            <h4 style="margin-bottom: 10px">Viwanja - Dege</h4>
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">
+                                    Dege, Kigamboni, Dar es salaam</strong>
+                            </p>
+                            <h1 style="font-size: small; margin-bottom: 10px">TZSH 25,000/= @ sqm 1</h1>
+                            <p>
+                                Huduma za Umeme na maji zinapatikana, shule za msingi na sekondary zote zipo karibu
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
                     <div class="single-project">
                         <img src="{{ asset('images/house7.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
+                        <div class="project-details" style="padding: 25px">
                             <h4 style="margin-bottom: 10px">Viwanja - Golani</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Kigamboni, Dar es salaam
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">
+                                    Kigamboni, Dar es salaam</strong>
                             </p>
                             <h1 style="font-size: small; margin-bottom: 10px">TZSH 7000/= @ sqm 1</h1>
+                            <p>
+                                Huduma za Umeme na maji zinapatikana, shule za msingi na sekondary zote zipo karibu
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="item" style="background-color: #27A7AA">
+                <div class="item">
                     <div class="single-project">
                         <img src="{{ asset('images/house8.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
+                        <div class="project-details" style="padding: 25px">
                             <h4 style="margin-bottom: 10px">Viwanja - Bagamoyo</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Makurunge, Bagamoyo
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">
+                                    Makurunge, Bagamoyo</strong>
                             </p>
                             <h1 style="font-size: small; margin-bottom: 10px">TZSH 1,500,000/= @ acre 1</h1>
+                            <p>
+                                Makurunge, Biringo,Mto upo karibu. Ardhi ina rutuba ,mazao yote yanakubali.
+                                Km 18 kutoka bagamoyo mjini, km 4 kutoka bagamoyo road.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="item" style="background-color: #27A7AA">
+                <div class="item">
                     <div class="single-project">
                         <img src="{{ asset('images/house9.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
+                        <div class="project-details" style="padding: 25px">
                             <h4 style="margin-bottom: 10px">Viwanja - Bunju</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Bunju, Mkwandara, Dar es salaam
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">
+                                    Bunju, Mkwandara, Dar es salaam</strong>
                             </p>
                             <h1 style="font-size: small; margin-bottom: 10px">TZSH 600,000/= @ acre 1</h1>
+                            <p>
+                                Umbali ni km 15 kutoka barabara kuu
+                                Bei ni 600,000/= per 1 acre
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="item" style="background-color: #27A7AA">
+                <div class="item">
                     <div class="single-project">
                         <img src="{{ asset('images/house10.jpg') }}" alt="" >
-                        <div class="" style="padding: 25px">
+                        <div class="project-details" style="padding: 25px">
                             <h4 style="margin-bottom: 10px">Viwanja - Kibaha</h4>
-                            <p>
-                                <i class="fa fa-map-marker"></i> Kibaha, Pwani
+                            <p style="color: #27A7AA">
+                                <i class="fa fa-map-marker"></i> <strong class="text-danger">Kibaha, Pwani</strong>
                             </p>
                             <h1 style="font-size: small; margin-bottom: 10px">TZSH 10,000/= @ sqm 1</h1>
+                            <p>
+                                Eneo lipo umbali wa km 4 kutoka lami
+                                Bei ni Tsh 10,000/= per sqm
+                            </p>
                         </div>
                     </div>
                 </div>
